56c838794608c72767ca64ed1365d3627bf3e70e
Vasni Jaimes <vasnijaimes@Vasnis-MacBook-Pro.local>
2021-02-15 09:06:34 -0600

    front

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,30 +12,30 @@ if (slide) {
 }
 
 
-// let menu = document.querySelector('.burger');
-// if (menu) {
-// 	menu.addEventListener('click', function(e) {
-// 		e.preventDefault();
-// 		document.querySelector('nav').classList.add('active');
-// 	})
-// }
+let menu = document.querySelector('.burger');
+if (menu) {
+	menu.addEventListener('click', function(e) {
+		e.preventDefault();
+		document.querySelector('nav').classList.add('active');
+	})
+}
 
-// let close = document.querySelector('nav .close');
-// if (close) {
-// 	close.addEventListener('click', function(e) {
-// 		e.preventDefault();
-// 		document.querySelector('nav').classList.remove('active');
-// 		document.querySelector('.contacto').style.display = "none";
-// 	})
-// }
+let close = document.querySelector('nav .close');
+if (close) {
+	close.addEventListener('click', function(e) {
+		e.preventDefault();
+		document.querySelector('nav').classList.remove('active');
+		document.querySelector('.contacto').style.display = "none";
+	})
+}
 
 
-// //EFECTO DE ALICK AL MENU
-// let itemMenu = document.querySelectorAll('header nav a');
-// if (itemMenu) {
-// 	for (var i = 0; i <= itemMenu.length - 1; i++) {
-// 		itemMenu[i].addEventListener('click', function(e) {
-// 			$('nav').removeClass();
-// 		});
-// 	}
-// }
+//EFECTO DE ALICK AL MENU
+let itemMenu = document.querySelectorAll('header nav a');
+if (itemMenu) {
+	for (var i = 0; i <= itemMenu.length - 1; i++) {
+		itemMenu[i].addEventListener('click', function(e) {
+			$('nav').removeClass();
+		});
+	}
+}
