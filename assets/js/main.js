8c5f98f24e8d92891fd5d6501e8899415b8e33f5
Vasni Jaimes <vasnijaimes@Vasnis-MacBook-Pro.local>
2021-02-16 02:55:47 -0600

    back

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,7 @@ if (close) {
 	close.addEventListener('click', function(e) {
 		e.preventDefault();
 		document.querySelector('nav').classList.remove('active');
-		document.querySelector('.contacto').style.display = "none";
+		//document.querySelector('.contacto').style.display = "none";
 	})
 }
 
@@ -38,4 +38,45 @@ if (itemMenu) {
 			$('nav').removeClass();
 		});
 	}
+}
+
+
+
+//CLICK PARA ABRIR UN POPUP EMPRESAS
+const quepago = document.querySelector('section.quepago button.empresa');
+if (quepago) {
+	quepago.addEventListener('click', function(e) {
+		e.preventDefault();
+		document.querySelector('.poup-empresas').style.display = 'flex';
+	});
+}
+
+//CLICK PARA CERRAR UN POPUP EMPRESAS
+const poup_empresas = document.querySelector('div.poup-empresas .close');
+if (poup_empresas) {
+	poup_empresas.addEventListener('click', function(e) {
+		document.querySelector('.poup-empresas').style.display = 'none';
+	})
+}
+
+
+
+
+
+
+//CLICK PARA ABRIR UN POPUP COLABORADORES
+const quepagoColab = document.querySelector('section.quepago button.empleado');
+if (quepagoColab) {
+	quepagoColab.addEventListener('click', function(e) {
+		e.preventDefault();
+		document.querySelector('.poup-colaboradores').style.display = 'flex';
+	});
+}
+
+//CLICK PARA CERRAR UN POPUP COLABORADORES
+const poup_empleado = document.querySelector('div.poup-colaboradores .close');
+if (poup_empleado) {
+	poup_empleado.addEventListener('click', function(e) {
+		document.querySelector('.poup-colaboradores').style.display = 'none';
+	})
 }
